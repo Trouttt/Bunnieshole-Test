17572175f9c8e1353d637548868f7ab613a962c7
Trouttt <44469295+Trouttt@users.noreply.github.com>
2021-04-16 18:52:38 -0300

    Added: connection with mongodb

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,11 @@
 /* eslint-disable no-console */
-import express from "express";
+import express from 'express';
+import mongoose from 'mongoose';
+import connect from './services/CreateConnectionService';
+
+connect(); // conectando com o mongodb
 
 const app = express();
 
-app.get("/", (request, response) => response.json({ message: "heloworld" }));
-app.listen(3333, () => console.log("oi"));
+app.get('/', (request, response) => response.json({ message: 'heloworld' }));
+app.listen(3333, () => console.log('oi'));
---
+++ server/src/services/CreateConnectionService.ts
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+import mongoose from 'mongoose';
+
+interface Request {
+  db: string;
+}
+
+const connect = () => {
+  mongoose
+    .connect('mongodb://localhost:27017/bunnies', { useNewUrlParser: true })
+    .then(() => {
+      return console.info(`Successfully connected`);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+export default connect;
+// mongodb://localhost:27017/bunnies
