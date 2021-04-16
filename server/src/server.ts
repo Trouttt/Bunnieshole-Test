efc851e5280ade14bff56dc423453105ccefcf9e
Trouttt <44469295+Trouttt@users.noreply.github.com>
2021-04-16 20:23:25 -0300

    Update: server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
 import express from 'express';
 import connect from './services/CreateConnectionService';
+import routes from './routes/index';
 
 connect(); // conectando com o mongodb
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'heloworld' }));
+app.use('/', routes);
+
 app.listen(3333, () => console.log('oi'));
