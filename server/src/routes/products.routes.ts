435bd4bdf3cde7c7f24c090bd8fced00a31f5051
Trouttt <44469295+Trouttt@users.noreply.github.com>
2021-04-17 14:11:44 -0300

    Update: added route POST

--- server/src/routes/products.routes.ts
+++ server/src/routes/products.routes.ts
@@ -1,9 +1,29 @@
+/* eslint-disable no-console */
 import { Router } from 'express';
+import CreateProductService from '../services/Product/CreateProductService';
 
 const productsRouter = Router();
 
 productsRouter.get('/', async (request, response) => {
-  return response.json({ message: 'salve salve familia' });
+  return response.json({ message: 'Hai' });
 });
+productsRouter.post('/', async (request, response) => {
+  try {
+    const { name, type, value, rating, thumbmail } = request.body;
 
+    const createProduct = new CreateProductService();
+
+    const product = await createProduct.execute({
+      name,
+      type,
+      value,
+      rating,
+      thumbmail,
+    });
+
+    return response.json(product);
+  } catch (err) {
+    return response.json(err);
+  }
+});
 export default productsRouter;
