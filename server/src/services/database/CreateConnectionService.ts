116cc39ec7441d1394fa4d53636d48256b0146fc
Trouttt <44469295+Trouttt@users.noreply.github.com>
2021-04-17 14:05:15 -0300

    Update: added conf 'useUnifiedTopology'

---
+++ server/src/services/database/CreateConnectionService.ts
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+import mongoose from 'mongoose';
+
+const connect = () => {
+  mongoose
+    .connect('mongodb://localhost:27017/bunnies', { useNewUrlParser: true })
+    .then(() => {
+      return console.info(`Successfully connected`);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+mongoose.set('useUnifiedTopology', true);
+
+export default connect;
