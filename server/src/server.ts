01a7afad28c4af44eebf0c3da542fefcdb5da123
Trouttt <44469295+Trouttt@users.noreply.github.com>
2021-04-17 14:18:15 -0300

    Update: added express.json() in server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,13 @@
 /* eslint-disable no-console */
 import express from 'express';
-import connect from './services/CreateConnectionService';
+import connect from './services/database/CreateConnectionService';
 import routes from './routes/index';
 
 connect(); // conectando com o mongodb
 
 const app = express();
 
+app.use(express.json());
 app.use('/', routes);
 
-app.listen(3333, () => console.log('oi'));
+app.listen(3333, () => console.log('Server on'));
